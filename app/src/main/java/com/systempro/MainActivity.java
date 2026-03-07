1bddaf80b0e8e63ebb929eae014c2454ecfdb3c6
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-07 08:14:47 +0500

    Structure Setup: app/src/main/java/com/systempro/MainActivity.java

--- app/src/main/java/com/systempro/MainActivity.java
+++ app/src/main/java/com/systempro/MainActivity.java
@@ -1,7 +1,6 @@
 package com.systempro;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnAction = findViewById(R.id.btnAction);
-        btnAction.setOnClickListener(v -> 
-            Toast.makeText(MainActivity.this, "جاوا ایکٹیویٹڈ!", Toast.LENGTH_SHORT).show()
-        );
+        
+        findViewById(R.id.mainButton).setOnClickListener(v -> {
+            Toast.makeText(this, "System Pro: Operations Online", Toast.LENGTH_SHORT).show();
+        });
     }
 }
