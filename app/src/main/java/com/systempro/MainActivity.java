fb051c5a582583451619155b0727501d5cabe7dc
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-07 07:50:06 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/MainActivity.java
+++ app/src/main/java/com/systempro/MainActivity.java
@@ -1,14 +1,19 @@
 package com.systempro;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // یہ لائن آپ کی XML فائل کو جاوا سے جوڑتی ہے
         setContentView(R.layout.activity_main);
+
+        Button btnAction = findViewById(R.id.btnAction);
+        btnAction.setOnClickListener(v -> 
+            Toast.makeText(MainActivity.this, "جاوا ایکٹیویٹڈ!", Toast.LENGTH_SHORT).show()
+        );
     }
 }
