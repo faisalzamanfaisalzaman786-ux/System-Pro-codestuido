985df91b77eab8d5e3d9359b986c8f513286a0c6
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-07 07:26:45 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/MainActivity.java
+++ app/src/main/java/com/systempro/MainActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // یہ لائن آپ کی XML فائل کو جاوا سے جوڑتی ہے
         setContentView(R.layout.activity_main);
     }
 }
