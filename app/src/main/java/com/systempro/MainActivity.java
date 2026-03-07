e7cd74bf1da296e6b416225b76256bb490a5f5bf
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-07 08:52:28 +0500

    System Pro Power Update

--- app/src/main/java/com/systempro/MainActivity.java
+++ app/src/main/java/com/systempro/MainActivity.java
@@ -1,7 +1,9 @@
 package com.systempro;
-
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.provider.Settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,9 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        findViewById(R.id.mainButton).setOnClickListener(v -> {
-            Toast.makeText(this, "System Pro: Operations Online", Toast.LENGTH_SHORT).show();
-        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(this)) {
+                Intent it = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                it.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(it);
+            }
+        }
     }
 }
