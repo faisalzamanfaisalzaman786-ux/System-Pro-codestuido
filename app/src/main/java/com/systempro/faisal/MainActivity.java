eacc0b7767af4b48eb41c280113e87e52c753937
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-09 19:20:21 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/faisal/MainActivity.java
+++ app/src/main/java/com/systempro/faisal/MainActivity.java
@@ -1,1 +1,16 @@
 
+package com.system.pro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // یہاں ہم فی الحال سادہ کوڈ رکھ رہے ہیں تاکہ بلڈ کامیاب ہو
+        android.widget.TextView tv = new android.widget.TextView(this);
+        tv.setText("System Pro is Running!");
+        setContentView(tv);
+    }
+}
