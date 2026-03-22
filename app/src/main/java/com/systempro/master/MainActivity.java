7139f76a5549abdb49fb897569792d31725ac4d1
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-22 22:01:21 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/master/MainActivity.java
+++ app/src/main/java/com/systempro/master/MainActivity.java
@@ -1,28 +1,14 @@
-// ضروری امپورٹس شامل کریں
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.widget.ImageView;
+package com.systempro.master;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
-// کلاس کے اندر یہ میتھڈ اور ویری ایبلز شامل کریں
-private static final int CAMERA_REQUEST = 1888;
-private ImageView imageView;
-
-// onCreate کے اندر بٹن کلک میں یہ لاجک ڈالیں
-imageView = findViewById(R.id.capturedImage);
-myButton.setOnClickListener(new View.OnClickListener() {
+public class MainActivity extends Activity {
     @Override
-    public void onClick(View v) {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-    }
-});
-
-// تصویر واپس لینے کے لیے یہ فنکشن شامل کریں
-@Override
-protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
-        imageView.setImageBitmap(photo);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Net Build Success!");
+        setContentView(tv);
     }
 }
