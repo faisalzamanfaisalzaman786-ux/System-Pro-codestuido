99ba67414e9427c6bb0430ab527f77d6ac3f089b
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-22 21:44:36 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/master/MainActivity.java
+++ app/src/main/java/com/systempro/master/MainActivity.java
@@ -1,1 +1,24 @@
 
+package com.systempro.master;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.actionButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "System Pro: Action Triggered!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
