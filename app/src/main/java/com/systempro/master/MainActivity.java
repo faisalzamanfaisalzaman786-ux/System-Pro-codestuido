75496d3624e30ad63d53be57ecb5cc93276f413a
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-03-23 07:20:49 +0500

    Update MainActivity.java

--- app/src/main/java/com/systempro/master/MainActivity.java
+++ app/src/main/java/com/systempro/master/MainActivity.java
@@ -1,25 +1,12 @@
 package com.systempro.master;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-import androidx.appcompat.app.AppCompatActivity;
-// مخصوص R پیکیج کو امپورٹ کرنا ضروری ہے
-import com.systempro.master.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ImageView imageView;
-
+public class MainActivity extends Activity {
     @Override
-    protected void Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // یہاں اب R پیکیج کا ایرر نہیں آئے گا
         setContentView(R.layout.activity_main);
-
-        imageView = findViewById(R.id.capturedImage);
-        Button btn = findViewById(R.id.actionButton);
-        
-        // آپ کی مزید لاجک یہاں آئے گی
     }
 }
