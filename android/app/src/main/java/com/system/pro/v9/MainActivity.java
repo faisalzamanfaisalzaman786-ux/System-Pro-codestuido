a58b625c6110cc43d477830ea4d03b87cc3f9fd6
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-04-18 21:07:00 +0500

    Update MainActivity.java

--- android/app/src/main/java/com/system/pro/v9/MainActivity.java
+++ android/app/src/main/java/com/system/pro/v9/MainActivity.java
@@ -1,1 +1,6 @@
-hfjcxhjj
+package com.system.pro.v9; // یہ پیکیج نیم آپ کے مینی فیسٹ سے میچ کرنا چاہیے
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
