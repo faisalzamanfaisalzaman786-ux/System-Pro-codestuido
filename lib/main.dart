c011986142dceeace279a4f5efee0af2098ca225
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-04-18 17:51:22 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("System Pro")),
-        body: const Center(child: Text("Flutter Build Successful!")),
-      ),
-    );
-  }
-}
+void main() => runApp(const MaterialApp(
+  home: Scaffold(body: Center(child: Text("System Pro Built Successfully!"))),
+));
