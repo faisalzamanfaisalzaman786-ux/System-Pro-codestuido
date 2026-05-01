f7287bd3ee0a1f948514a9d00434f65ce25d5f0e
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-01 16:57:40 +0500

    Update app_logic.java

--- templates/app_logic.java
+++ templates/app_logic.java
@@ -1,1 +1,13 @@
 
+package com.system.pro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
