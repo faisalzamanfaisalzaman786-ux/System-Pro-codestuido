690839f8e0d51bcbc953e0567731d3e2738cb571
Muzammil-AI <muzammil@systempro.ai>
2026-05-02 10:35:50 +0000

    System Pro: Structure Update [Standard Java]

---
+++ app/src/main/java/com/faisal/systempro/MainActivity.java
@@ -0,0 +1,9 @@
+package com.faisal.systempro;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
