98a1e6c91c629ca909bd14a0ea4b20d4e89f02cd
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-03 06:47:42 +0500

    Update MainActivity.java

--- app/src/main/java/com/faisal/systempro/MainActivity.java
+++ app/src/main/java/com/faisal/systempro/MainActivity.java
@@ -1,9 +1,24 @@
-package com.faisal.systempro;
-import android.app.Activity;
+package com.system.pro;
+
 import android.os.Bundle;
-public class MainActivity extends Activity {
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        // یہ آپ کی index.html کو لوڈ کرے گا
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
