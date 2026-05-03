62d1d8d5b949a8fc0d29a3b680ad2ecc919e4fee
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-03 21:19:12 +0500

    Create MainActivity.java

---
+++ app/src/main/java/com/system/pro/MainActivity.java
@@ -0,0 +1,24 @@
+package com.system.pro;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        applyCustomTheme();
+        setContentView(R.layout.activity_main);
+    }
+
+    private void applyCustomTheme() {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+    }
+}
