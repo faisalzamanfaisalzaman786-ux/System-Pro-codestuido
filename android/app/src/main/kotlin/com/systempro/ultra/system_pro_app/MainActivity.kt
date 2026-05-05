e5858c1b4f54f33a0f2a539935a8465ad29a1f31
faisalzamanfaisalzaman786-ux <faisalzamanfaisalzaman786@gmail.com>
2026-05-05 14:34:06 +0500

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/systempro/ultra/system_pro_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/systempro/ultra/system_pro_app/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.systempro.ultra.system_pro_app
+package com.systempro.template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
